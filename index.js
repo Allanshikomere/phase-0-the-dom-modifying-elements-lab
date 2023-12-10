f1b82b92fdc20c7b5db6d9d6a8f5bf3d1b853adf
Allanshikomere <allanshikomere@gmail.com>
2023-12-10 11:49:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+document.addEventListener('DOMContentLoaded', function () {
+    // Find the main element with id 'main'
+    const mainElement = document.querySelector('main#main');
+  
+    if (mainElement) {
+      mainElement.parentNode.removeChild(mainElement);
+    }
+  });
+
+  function createNewHeader() {
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.textContent = 'Allan is the champion';
+    return newHeader;
+  }
+  
+  const newHeader = createNewHeader();
+  
+  document.body.appendChild(newHeader);
+  newHeader.innerHTML = "ALLAN is the champion";
